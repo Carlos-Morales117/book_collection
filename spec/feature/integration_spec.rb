fb672669bbfeab7d5336ff2bb43d98ee0a3c86e3
Carlos_Morales117 <carlos_morales117@live.com>
2022-02-03 16:18:08 -0600

    Fixed the index page and edited the integration tests, and the unit tests. Everything should be good to go

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,7 +1,46 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
+RSpec.describe 'Creating a book with only title', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+  end
+end
+
+RSpec.describe 'Creating a book with title and author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    #fill_in 'Author', with: 'J. K. Rowling'
+
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J. K. Rowling')
+  end
+end
+
+RSpec.describe 'Creating a book with title, author, and price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    #fill_in 'Price', with: '40.99'
+
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J. K. Rowling')
+    expect(page).to have_content('40.99')
+
+  end
+end
+
+RSpec.describe 'Creating a book with title, author, price, and published date', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
@@ -9,9 +48,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Price', with: '40.99'
 
    
-    select '2020', :from => 'book_published_date_1i'      # Year
-    select 'November', :from => 'book_published_date_2i'  # Month
-    select '16', :from => 'book_published_date_3i'        # Day
+   # select '2020', :from => 'book_published_date_1i'      # Year
+   # select 'November', :from => 'book_published_date_2i'  # Month
+   # select '16', :from => 'book_published_date_3i'        # Day
 
     click_on 'Create Book'
     visit books_path
